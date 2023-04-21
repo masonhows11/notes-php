689dee7d17a8f7edcf5a1ab538a4e1ddd2cb5cda
mason.hows11 <mason.hows11@gmail.com>
2023-04-21 17:19:42 +0430

    routes updated

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
 <?php
+//  if ($uri == '/') {
+//
+//    require 'controllers/home.php';
+//
+//  } else if ($uri == '/about') {
+//
+//    require 'controllers/about.php';
+//
+//  } else if ($uri == '/contact') {
+//
+//    require 'controllers/contact.php';
+//  }
 
 require 'functions.php';
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-if ($uri == '/') {
 
-    require 'controllers/home.php';
+$routes = [
+    '/' => 'controllers/home.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php'
+];
 
-} else if ($uri == '/about') {
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    http_response_code(404);
+    require  'views/404.view.php';
 
-    require 'controllers/about.php';
-
-} else if ($uri == '/contact') {
-
-    require 'controllers/contact.php';
+    //    echo 'Sorry not found';
+    //    die();
 }
