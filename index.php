fab5dd5eb6a9ca708630d94897235ed89d21cc17
mason.hows11 <mason.hows11@gmail.com>
2023-04-21 23:23:46 +0430

    ready for connect to database

--- index.php
+++ index.php
@@ -4,3 +4,19 @@
 require 'functions.php';
 
 require 'router.php';
+
+
+// connect to our mysql database
+
+$servername = "localhost";
+$username = "root";
+$password = "1289..//";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=demoDb", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
