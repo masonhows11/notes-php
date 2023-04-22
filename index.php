09ae3214dea8e7170ed06186e9bb7ab80f0273a9
mason.hows11 <mason.hows11@gmail.com>
2023-04-22 11:00:50 +0430

    note project start

--- index.php
+++ index.php
@@ -8,13 +8,14 @@ require 'router.php';
 require 'database.php';
 
 $config = require('config.php');
-$db = new Database($config['database']);
 
-$id = $_GET['id'];
-
-$query = "select * from posts where id = :id ";
-
-$posts = $db->query($query, ['id' => $id])->fetch();
+//$db = new Database($config['database']);
+//
+//$id = $_GET['id'];
+//
+//$query = "select * from posts where id = :id ";
+//
+//$posts = $db->query($query, ['id' => $id])->fetch();
 
 
 
