e5eb677bc15cb05e29f6ad43c5fc48d39748c378
mason.hows11 <mason.hows11@gmail.com>
2023-04-23 17:16:10 +0430

    server validation updated

--- controllers/post-create.php
+++ controllers/post-create.php
@@ -5,13 +5,23 @@ $db = new database($config['database']);
 
 $heading = 'Create post';
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-$db->query('insert into posts(title,description,user_id) values (:title,:description,:user_id)',[
-    'title' => $_POST['title'], 'description' => $_POST['description'] , 'user_id' => 10,
-    ]);
+    $errors = [];
 
-header("location:http://notes.test/posts");
+    if (strlen($_POST['title']) == 0 && strlen($_POST['body']) == 0) {
+        $errors['title'] = 'title is required';
+        $errors['body'] = 'body is required';
+    }
+
+    if (empty($errors)) {
+        $db->query('insert into posts(title,body,user_id) values (:title,:body,:user_id)', [
+            'title' => $_POST['title'], 'body' => $_POST['body'], 'user_id' => 10,
+        ]);
+        header("location:http://notes.test/posts");
+    }
+
+
 }
 
 require "views/post-create.view.php";
--- views/post-create.view.php
+++ views/post-create.view.php
@@ -6,14 +6,32 @@
     <div class="col-lg-5">
 
         <form  method="post" >
+
             <div class="mb-3">
                 <label for="title" class="form-label">title</label>
                 <input type="text" name="title" class="form-control" id="title">
+
+                <?php if(isset($errors['title'])) : ?>
+                    <div class="alert alert-danger">
+                        <?php echo $errors['title'] ?>
+                    </div>
+                <?php endif; ?>
+
             </div>
+
             <div class="mb-3">
-                <label for="desc" class="form-label">Example textarea</label>
-                <textarea class="form-control" name="description" id="desc" rows="3"></textarea>
+                <label for="body" class="form-label">Example textarea</label>
+                <textarea class="form-control" name="body" id="body" rows="3">
+                </textarea>
+
+                <?php if(isset($errors['body'])) : ?>
+                <div class="alert alert-danger">
+                    <?php echo $errors['body'] ?>
+                </div>
+                <?php endif; ?>
+
             </div>
+
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
 
