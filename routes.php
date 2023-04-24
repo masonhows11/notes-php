cedb645289a9e1f5e132c6faf749a5fa4d217945
mason.hows11 <mason.hows11@gmail.com>
2023-04-24 19:52:35 +0430

    refactor notes project

--- routes.php
+++ routes.php
@@ -2,9 +2,11 @@
 
 return [
     '/' => 'controllers/home.php',
-    '/posts' => 'controllers/posts.php',
-    '/post' => 'controllers/post.php',
-    '/post/create' => 'controllers/post-create.php',
+
+    '/posts' => 'controllers/posts/index.php',
+    '/post' => 'controllers/posts/show.php',
+    '/post/create' => 'controllers/posts/create.php',
+
     '/about' => 'controllers/about.php',
     '/contact' => 'controllers/contact.php'
 ];
