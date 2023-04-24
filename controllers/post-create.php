16aa45b70be0afd2dcd8075f5ba02a9e95f7d601
mason.hows11 <mason.hows11@gmail.com>
2023-04-24 18:58:14 +0430

    add class for validation

--- controllers/post-create.php
+++ controllers/post-create.php
@@ -1,7 +1,10 @@
 <?php
 
+
+require 'services/Validator.php';
+
 $config = require('config.php');
-$db = new database($config['database']);
+$db = new Database($config['database']);
 
 $heading = 'Create post';
 
@@ -10,19 +13,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors_title = [];
     $errors_body = [];
    
-   
-    // dd($_POST['body']);
-    
-    if (empty($_POST['title']) && empty($_POST['body'])) {
 
+    $validator = new Validator();
+
+    if ($validator->string($_POST['title']) && $validator->string($_POST['body'])) {
+        $errors_title['title'] = 'title is required';
+        $errors_body['body'] = 'body is required';
+    }
+
+    // dd($_POST['body']);
+   /* if (empty($_POST['title']) && empty($_POST['body'])) {
         $errors_title['title'] = 'title is required';
         $errors_body['body'] = 'body is required';
     }
 
     if (strlen($_POST['body']) > 10) {
-
         $errors_body['body'] = 'maximum characters is 10';
-    }
+    }*/
 
     if (empty($errors_title) && empty($errors_body)) {
         $db->query('insert into posts(title,body,user_id) values (:title,:body,:user_id)', [
---
+++ services/Validator.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Validator
+{
+
+
+    public function string($value)
+    {
+
+        return strlen(trim($value)) === 0;
+
+    }
+
+
+}
