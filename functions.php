3bdb04dd51d4f0e450555ac92bac2a5583bcaa01
mason.hows11 <mason.hows11@gmail.com>
2023-04-25 18:45:44 +0430

    refactor

--- functions.php
+++ functions.php
@@ -35,7 +35,8 @@ function base_path($path)
     return BASE_PATH . $path;
 }
 
-function view($path)
+function view($path, $attributes = [])
 {
+    extract($attributes);
     require base_path('views/' . $path);
 }
