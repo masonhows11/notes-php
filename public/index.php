9315b893f0159f2da39d847d52a8d11513e02944
mason.hows11 <mason.hows11@gmail.com>
2023-04-25 20:42:42 +0430

    refactor updated

--- public/index.php
+++ public/index.php
@@ -1,13 +1,18 @@
 <?php
 
 // get our project path
-const BASE_PATH = __DIR__ .'/../';
+const BASE_PATH = __DIR__ . '/../';
 
 
 require BASE_PATH . 'functions.php';
 
-require base_path('Database.php');
+spl_autoload_register(function ($class) {
 
-require base_path('Response.php');
+    require base_path("core/" . $class, '.php');
+});
+
+// require base_path('Database.php');
+
+// require base_path('Response.php');
 
 require base_path('router.php');
