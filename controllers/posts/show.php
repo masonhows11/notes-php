c716902a574b703ecec239e2fd7ca53652d280cf
mason.hows11 <mason.hows11@gmail.com>
2023-04-26 19:07:17 +0430

    delete record updated

--- controllers/posts/show.php
+++ controllers/posts/show.php
@@ -9,16 +9,28 @@ $post = $db->query('select * from posts where id = :id ', ['id' => $_GET['id']])
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $db->query('delete from posts where id = :id', [
+
+        'id' => $_GET['id']
+
+    ]);
+
+    header('location: /posts');
+
+    exit();
+} else {
+
+    // if ($post['user_id'] !== 7) {
+    //     abort(Response::FORBIDDEN);
+    // }
+
+
+
+    view("posts/show.view.php", [
+
+        'heading' => 'My Post',
+        'post' => $post
+
+    ]);
 }
-// if ($post['user_id'] !== 7) {
-//     abort(Response::FORBIDDEN);
-// }
-
-
-
-view("posts/show.view.php", [
-
-    'heading' => 'My Post',
-    'post' => $post
-
-]);
