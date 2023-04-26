e6dd3bb493a62d2f3b5dcd179a82242a4c229b5e
mason.hows11 <mason.hows11@gmail.com>
2023-04-26 21:51:42 +0430

    delete resource with route & controller add controller

--- controllers/posts/destroy.php
+++ controllers/posts/destroy.php
@@ -1,1 +1,28 @@
 <?php
+
+use core\database;
+
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+$post = $db->query('select * from posts where id = :id ', ['id' => $_GET['id']])->findOrFail();
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $db->query('delete from posts where id = :id', [
+        'id' => $_GET['id']
+    ]);
+    header('location: /posts');
+    exit();
+
+} else {
+    // if ($post['user_id'] !== 7) {
+    //     abort(Response::FORBIDDEN);
+    // }
+    view("posts/show.view.php", [
+        'heading' => 'My Post',
+        'post' => $post
+    ]);
+}
+
