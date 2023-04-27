1288f31051fcf8dd9542f7ebc899fdaad4755285
mason.hows11 <mason.hows11@gmail.com>
2023-04-27 07:40:09 +0430

    service container updated

--- core/Container.php
+++ core/Container.php
@@ -7,13 +7,24 @@ namespace core;
 class Container
 {
 
-    public function bind()
-    {
+    protected $bindings = [];
 
+    public function bind($key, $resolver)
+    {
+        $this->bindings[$key] = $resolver;
     }
 
-    public function resolve()
+    public function resolve($key)
     {
+        if (!array_key_exists($key, $this->bindings)) {
+
+            throw new \Exception("No matching binding found for {$key}");
+
+        }
+
+        $resolver = $this->bindings[$key];
+
+        return call_user_func($resolver);
 
     }
 
