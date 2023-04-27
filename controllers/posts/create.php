10880dc51d3ea9ae7bcbb6d3b53c3ea62be62968
mason.hows11 <mason.hows11@gmail.com>
2023-04-27 06:30:26 +0430

    store resource in new route system fixed 80%

--- controllers/posts/create.php
+++ controllers/posts/create.php
@@ -8,7 +8,7 @@
 view("posts/create.view.php", [
 
     'heading' => 'Create post',
-    'errors_title' => $errors_title,
-    'errors_body' => $errors_body
+    'errors_title' => [],
+    'errors_body' => []
 
 ]);
--- controllers/posts/store.php
+++ controllers/posts/store.php
@@ -11,21 +11,39 @@ $db = new Database($config['database']);
 $errors_title = [];
 $errors_body = [];
 
-if (!Validator::string($_POST['title'], 1, 10)) {
-
-    $errors_title['title'] = 'title is required & between 1 & 10 characters';
+if (!Validator::string($_POST['title'], 1, 30)) {
+    $errors_title['title'] = 'title is required & between 1 & 30 characters';
 }
 
-if (!Validator::string($_POST['body'], 1, 10)) {
-
-    $errors_body['body'] = 'body is required & between 1 & 10 characters';
+if (!Validator::string($_POST['body'], 1, 30)) {
+    $errors_body['body'] = 'body is required & between 1 & 30 characters';
 }
 
+if(! empty($errors_title)){
+
+   return view("posts/create.view.php", [
+        'heading' => 'Create post',
+        'errors_title' => $errors_title,
+        'errors_body' => $errors_body
+
+    ]);
+}
+
+if(! empty($errors_body)){
+
+    return view("posts/create.view.php", [
+        'heading' => 'Create post',
+        'errors_title' => $errors_title,
+        'errors_body' => $errors_body
+
+    ]);
+}
 
 if (empty($errors_title) && empty($errors_body)) {
     $db->query('insert into posts(title,body,user_id) values (:title,:body,:user_id)', [
         'title' => $_POST['title'], 'body' => $_POST['body'], 'user_id' => 10,
     ]);
     header("location: /posts");
+    die();
 }
 
