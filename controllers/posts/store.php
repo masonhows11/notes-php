19a282dc20737af6fcc238a2ea653be882237a79
mason.hows11 <mason.hows11@gmail.com>
2023-04-27 06:24:12 +0430

    store resource in new route system updated

--- controllers/posts/store.php
+++ controllers/posts/store.php
@@ -1,3 +1,31 @@
 <?php
 
-dd('hello');
+use Core\Validator;
+use Core\Database;
+
+//require base_path("core/".'Validator.php');
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$errors_title = [];
+$errors_body = [];
+
+if (!Validator::string($_POST['title'], 1, 10)) {
+
+    $errors_title['title'] = 'title is required & between 1 & 10 characters';
+}
+
+if (!Validator::string($_POST['body'], 1, 10)) {
+
+    $errors_body['body'] = 'body is required & between 1 & 10 characters';
+}
+
+
+if (empty($errors_title) && empty($errors_body)) {
+    $db->query('insert into posts(title,body,user_id) values (:title,:body,:user_id)', [
+        'title' => $_POST['title'], 'body' => $_POST['body'], 'user_id' => 10,
+    ]);
+    header("location: /posts");
+}
+
